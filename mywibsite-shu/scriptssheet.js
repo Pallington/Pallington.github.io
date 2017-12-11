d01ddb3a5926356cf8b607b61d1d09934992bac7
Andy Shu <andy_shu@live.com>
2017-12-11 20:16:11 +0000

    bio test

--- mywibsite-shu/scriptssheet.js
+++ mywibsite-shu/scriptssheet.js
@@ -1,3 +1,8 @@
-var img = document.getElementById('IMG_1349.jpg'); 
+var img = document.getElementById('ss2'); 
 var width1 = img.clientWidth;
 var height1 = img.clientHeight;
+var img2 = document.getElementById('ss3'); 
+var width2 = img.clientWidth;
+var height2 = img.clientHeight;
+var spacing = 200 + height1;
+img.style.top = spacing+'px';
