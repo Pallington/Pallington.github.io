ad49bf314c30dcf4de7e1c36babca87c46db37bb
Andy Shu <andy_shu@live.com>
2017-12-13 19:20:49 +0000

    email fix

--- mywibsite-shu/scriptssheet.js
+++ mywibsite-shu/scriptssheet.js
@@ -1,8 +1,8 @@
 var img = document.getElementById('ss2'); 
-var width1 = img.clientWidth;
-var height1 = img.clientHeight;
+var width1 = img.clientWidth - 'px';
+var height1 = img.clientHeight - 'px';
 var img2 = document.getElementById('ss3'); 
 var width2 = img.clientWidth;
 var height2 = img.clientHeight;
-var spacing = 200 + height1;
+var spacing = 400 + height1;
 img.style.top = spacing+'px';
